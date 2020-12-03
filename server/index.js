5a80ef31240c3353a02f8e075982e341022d1f20
Афанасьев Дмитрий Васильевич <Afanasyev.D.Vasi@sberbank.ru>
2020-12-03 21:39:52 +0700

    add env

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const Koa = require('koa');
 const middleware = require('./middleware');
+require('dotenv').config();
+const PORT = process.env.NODE_SERVER_PORT || 3000;
 
 const app = new Koa();
 
 middleware(app);
 
-app.listen(3000, () => {
-  console.log('Listening @ http://localhost:3000/');
+app.listen(PORT, () => {
+  console.log(`Listening @ http://localhost:${PORT}/`);
 });
