ee0ca5a902890fda16513c2aa4152de60be4bd13
Афанасьев Дмитрий Васильевич <Afanasyev.D.Vasi@sberbank.ru>
2020-12-13 20:50:54 +0700

    новая мидлварь

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,30 +1,41 @@
 import {NOT_FOUND} from 'redux-first-router';
+import {CALL_API, LOAD_MY_DATA_REQUEST, LOAD_MY_DATA_SUCCESS, LOAD_MY_DATA_FAILURE} from '../types';
 
 // try dispatching these from the redux devTools
 
+export const getVideoActionCreator = (category, jwToken) => (dispatch) =>
+    dispatch({
+        [CALL_API]: {
+            types: [LOAD_MY_DATA_REQUEST, LOAD_MY_DATA_SUCCESS, LOAD_MY_DATA_FAILURE],
+            method: 'GET',
+            endpoint: `/videos/${category}`,
+            payload: {category, jwToken},
+        },
+    });
+
 export const goToPage = (type, category) => ({
-  type,
-  payload: category && {category},
+    type,
+    payload: category && {category},
 });
 
 export const goHome = () => ({
-  type: 'HOME',
+    type: 'HOME',
 });
 
 export const goToAdmin = () => ({
-  type: 'ADMIN',
+    type: 'ADMIN',
 });
 
 export const notFound = () => ({
-  type: NOT_FOUND,
+    type: NOT_FOUND,
 });
 
 export const visitCategory = (category) => ({
-  type: 'LIST',
-  payload: {category},
+    type: 'LIST',
+    payload: {category},
 });
 
 export const visitVideo = (slug) => ({
-  type: 'VIDEO',
-  payload: {slug},
+    type: 'VIDEO',
+    payload: {slug},
 });
--- src/reducers/videosByCategory.js
+++ src/reducers/videosByCategory.js
@@ -1,11 +1,15 @@
-export default (state = {}, action = {}) => {
-  if (action.type === 'VIDEOS_FETCHED') {
-    const {category, videos} = action.payload;
-    const slugs = videos.map((video) => video.slug);
-    return {...state, [category]: slugs};
-  }
+import {LOAD_MY_DATA_SUCCESS} from '../types';
 
-  return state;
+export default (state = {}, action = {}) => {
+    // return state
+    switch (action.type) {
+        case LOAD_MY_DATA_SUCCESS:
+            const {category, response: videos} = action.payload;
+            const slugs = videos.map((video) => video.slug);
+            return {...state, [category]: slugs};
+        default:
+            return state;
+    }
 };
 
 // eg: { fp: ['slug-1', 'slug-2'], 'react-redux': ['slug-etc'] }
